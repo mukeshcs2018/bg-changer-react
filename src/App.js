3ad8b00447fbfbb5595cea1e47039741667ea4fc
mukeshcs2018 <mukesh.cs2018@gmail.com>
2024-01-21 21:23:03 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("black");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="w-full h-screen duration-1000"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-2xl animate-pulse color-white text-white"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-2xl animate-pulse text-white"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-2xl animate-pulse text-white"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor("yellow")}
+            className="outline-none px-4 py-1 rounded-2xl animate-pulse text-white"
+            style={{ backgroundColor: "yellow" }}
+          >
+            Yellow
+          </button>
+          <button
+            onClick={() => setColor("orange")}
+            className="outline-none px-4 py-1 rounded-2xl animate-pulse text-white"
+            style={{ backgroundColor: "orange" }}
+          >
+            Orange
+          </button>
+          <button
+            onClick={() => setColor("violet")}
+            className="outline-none px-4 py-1 rounded-2xl animate-pulse text-white"
+            style={{ backgroundColor: "violet" }}
+          >
+            Violet
+          </button>
+          <button
+            onClick={() => setColor("magenta")}
+            className="outline-none px-4 py-1 rounded-2xl animate-pulse text-white"
+            style={{ backgroundColor: "magenta" }}
+          >
+            Magenta
+          </button>
+          <button
+            onClick={() => setColor("black")}
+            className="outline-none px-4 py-1 rounded-2xl animate-pulse text-white"
+            style={{ backgroundColor: "black" }}
+          >
+            Black
+          </button>
+          <button
+            onClick={() => setColor("pink")}
+            className="outline-none px-4 py-1 rounded-2xl animate-pulse text-white"
+            style={{ backgroundColor: "pink" }}
+          >
+            Pink
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
